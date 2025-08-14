595c3346472230092b88e0941992ff87b57b4542
Germthe <42160602-Germthe@users.noreply.replit.com>
2025-08-14 16:26:43 +0000

    Restored to '4ea078702242f88fd6aa59a92115d3caa2ad82c9'
    Replit-Restored-To: 4ea078702242f88fd6aa59a92115d3caa2ad82c9

--- client/src/pages/question-set-practice-optimized.tsx
+++ client/src/pages/question-set-practice-optimized.tsx
@@ -631,7 +631,7 @@ export default function QuestionSetPractice() {
         {/* Right Main Content - Question */}
         <div className="flex-1 flex flex-col overflow-hidden">
           <div className="flex-1 px-4 pt-0 pb-4 lg:p-4 flex items-center justify-center min-h-0">
-            <div className="w-full max-w-4xl mx-auto" style={{ height: 'calc(100vh - 180px)' }}>
+            <div className="w-full max-w-4xl mx-auto h-full">
               {currentQuestion ? (
                 <QuestionCard
                   question={{
